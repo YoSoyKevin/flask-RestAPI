52ca52608d86c2aaf38aeefcbcbb4d27b2bf4f09
YoSoyKevin <kevinshimizu1@gmai.com>
2023-03-09 23:42:30 -0500

    Creando api con FLASK

---
+++ src/config.py
@@ -0,0 +1,10 @@
+class developmentConfig():
+    DEBUG=True
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = '1234'
+    MYSQL_DB ='api_flask'
+
+config={
+    'development':developmentConfig
+}    
